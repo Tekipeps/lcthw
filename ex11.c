32ba8832b2624879b90c9d72dd3db86b52593e94
Tekena Solomon <tekena157@gmail.com>
2021-01-04 00:27:50 +0100

    Add ex11.c

--- ex11.c
+++ ex11.c
@@ -2,16 +2,16 @@
 
 int main(int argc, char *argv[]){
   int i = 0;
-
-  for(i = 0; i < argc; i++){
+  while (i < argc){
     printf("arg %d: %s\n", i, argv[i]);
+    i++;
   }
-
-  char *states[] = {"Carlifonia", "Oregon", "Washington", "Texas", NULL};
+  char *states[] = {"California", "Oregon", "Washington", "Texas"};
   int num_states = 4;
-
-  for(i = 0; i < num_states; i++){
+  i = 0;
+  while(i < num_states){
     printf("state %d: %s\n", i, states[i]);
+    i++;
   }
   return 0;
 }
