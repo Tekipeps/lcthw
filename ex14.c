6cec8a40df288b80fe9775aec1acd3cef5050f51
Tekena Solomon <tekena157@gmail.com>
2021-01-04 23:57:53 +0100

    Done Exercises

--- ex14.c
+++ ex14.c
@@ -16,15 +16,11 @@ void print_letters(char arg[]){
   int i = 0;
   for(i = 0; arg[i] != '\0'; i++){
     char ch = arg[i];
-      if (can_print_it(ch)){
+      if (isalpha(ch) || isblank(ch)){
         printf("'%c' == %d ", ch, ch);
       }
   }
   printf("\n");
-}
-
-int can_print_it (char ch){
-  return isalpha(ch) || isblank(ch);
 }
 
 int main(int argc, char *argv[]){
