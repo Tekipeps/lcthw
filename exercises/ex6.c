febfad3b7f2f3cc26ddd75c6e1e3a6c89a68d6a3
Tekena Solomon <tekena157@gmail.com>
2021-02-05 20:48:41 +0100

    Redo ex6 and ex7

--- exercises/ex6.c
+++ exercises/ex6.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
   int distance = 100;
   float power = 2.345f;
   double super_power = 56789.4537;
   char initial = 'A';
-  char first_name[] =  "Bad";
-  char last_name[] = "Shaw";
+  int age = 18;
+  char first_name[] = "Tekena";
+  char last_name[] = "Solomon";
 
   printf("You are %d miles away. \n", distance);
   printf("You have %f levels of power. \n", power);
@@ -14,6 +16,7 @@ int main(int argc, char *argv[]){
   printf("I have an initial %c.\n", initial);
   printf("I have a first name %s. \n", first_name);
   printf("I have a last name %s \n", last_name);
-  printf("My whole name is %s %c. %s. \n ", initial, first_name, last_name);
+  printf("My whole name is %s %c. %s. \n ", first_name, initial, last_name);
+  // printf("My name is %s i am %d years old\n", first_name, age);
   return 0;
 }
--- exercises/ex7.c
+++ exercises/ex7.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[])
+{
   int bugs = 100;
   double bug_rate = 1.2;
 
-  printf("You have %d bugs at the imageinary rate of %f. \n", bugs, bug_rate);
+  printf("You have %d bugs at an imaginary rate of %f.\n", bugs, bug_rate);
 
-  long universe_of_defects = 1L * 1024L * 1024L * 1024L * 99999L * 999999999999L;
-  printf("The entire universe has %ld bugs. \n", universe_of_defects);
+  long universe_of_defect = 1L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L;
+  printf("The entire universe has %ld bugs.\n", universe_of_defect);
 
   double expected_bugs = bugs * bug_rate;
-  printf("You are expected to have %f bugs. \n", expected_bugs);
+  printf("You are expected to have %f bugs..\n", expected_bugs);
 
-  double part_of_the_universe =  expected_bugs / universe_of_defects;
-  printf("That is only a %e portion of the universe. \n", part_of_the_universe);
+  double part_of_the_universe = expected_bugs / universe_of_defect;
+  printf("This is only %e portion of the universe.\n", part_of_the_universe);
 
-  // this makes no sense, just a demo of something weird
   char nul_byte = '\0';
   int care_percentage = bugs * nul_byte;
-  printf("Which means you should care %d%%. \n", care_percentage);
-  printf("%c \n", nul_byte);
+  printf("Which means you should care %d%%.\n", care_percentage);
+
+  // printf("\nnul_byte: %%c -> %c; %%d -> %d; %%s -> %s\n", nul_byte, nul_byte, nul_byte);
+
   return 0;
 }
