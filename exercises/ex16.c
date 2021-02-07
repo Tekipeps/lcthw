bdc3adf1d3d610937c5e051b0f556fde518e5d81
Tekena Solomon <tekena157@gmail.com>
2021-02-07 08:30:23 +0100

    Redo ex16

--- exercises/ex16.c
+++ exercises/ex16.c
@@ -1,50 +1,52 @@
-#include <stdio.h> // include the standard input/output library
-#include <assert.h> // include the assert function for debugging
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-struct Person { // create a Person struct
-  char *name; // property name string
-  int age; // property age int
-  int height; // property height int
-  int weight; // property height int
+struct Person {
+  char *name;
+  int age;
+  int height;
+  int weight;
 };
 
-struct Person *Person_create(char *name, int age, int height, int weight){ //Person_create funtion
-  struct Person *who = malloc(sizeof(struct Person)); // create memory space for person
-  assert(who != NULL); // make sure who is valid memory space
-  who->name = strdup(name); // duplicate the string for name and add it to the structure
-  // to make sure the structure actually owns it
-  who->age = age;  // add the age to the structure
-  who->height = height; // add the height to the structure
-  who->weight = weight; // add the weight to the stucture
+struct Person *Person_create(char *name, int age, int height, int weight) {
+  struct Person *who = malloc(sizeof(struct Person));
+  assert(who != NULL);
 
-  return who; // return the address to the structure
+  who->name = strdup(name);
+  who->age = age;
+  who->height = height;
+  who->weight = weight;
+
+  return who;
 }
 
-void Person_destroy(struct Person *who){ // function to destroy person struct i.e cleanup from memory
-  assert(who != NULL); // ensure who is a defined pointer
-  free(who->name); // free the memory created for name using the strdup()
-  free(who); // free the memory created for the struct using malloc()
-}
-void Person_print(struct Person *who){ // display the details of person
-  printf("Name: %s\n", who->name); // print the name
-  printf("\tAge: %d\n", who->age); // print the age
-  printf("\tHeight: %d\n", who->height); // print the height
-  printf("\tWeight: %d\n", who->weight); // print the weight
+void Person_destroy(struct Person *who) {
+  assert(who != NULL);
+
+  free(who->name);
+  free(who);
 }
 
-int main(int argc, char *argv[]){
-  // make two people structures
-  struct Person *joe = Person_create("Joe Alex", 32,64,140); // create a person struct joe
-  struct Person *frank = Person_create("Frank Blank",20,72,180); // create a person struct frank
+void Person_print(struct Person *who) {
+  printf("Name: %s\n", who->name);
+  printf("\tAge: %d\n", who->age);
+  printf("\tHeight: %d\n", who->height);
+  printf("\tWeight: %d\n", who->weight);
+}
 
-  printf("Joe is at memory location %p\n", joe); // print the memory location of joe
-  Person_print(joe); // print the properties of joe
-  printf("Frank is at memory location %p\n", frank); // print the memory location of frank
-  Person_print(frank); // print the properties of frank
+int main(int argc, char *argv[]) {
+  struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
+  struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
 
-  // make everyone 20 years and print them again
+  printf("Joe is at memory location %p:\n", joe);
+  Person_print(joe);
+
+  printf("Frank is at memory location %p;\n", frank);
+  Person_print(frank);
+
+  // make everyone age 20 years and print them again
   joe->age += 20;
   joe->height -= 2;
   joe->weight += 40;
@@ -54,7 +56,7 @@ int main(int argc, char *argv[]){
   frank->weight += 20;
   Person_print(frank);
 
-  //destroy both of them so we clean up
+  // destroy them both so we cleanup
   Person_destroy(joe);
   Person_destroy(frank);
 
