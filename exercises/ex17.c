64f5e70d435189119b46e2ec9c0f8d27bd200654
Tekena Solomon <tekena157@gmail.com>
2021-03-07 21:43:51 +0100

    Finished problem: arbritrarily sized database

--- exercises/ex17.c
+++ exercises/ex17.c
@@ -10,12 +10,14 @@
 struct Address {
   int id;
   int set;
-  char name[MAX_DATA];
-  char email[MAX_DATA];
+  char *name;
+  char *email;
 };
 
 struct Database {
-  struct Address rows[MAX_ROWS];
+  size_t max_rows;
+  size_t max_data;
+  struct Address *rows;
 };
 
 struct Connection {
@@ -23,12 +25,32 @@ struct Connection {
   struct Database *db;
 };
 
-void die(const char *message) {
+void Database_close(struct Connection *conn) {
+  if (conn) {
+    for (size_t i = 0; i < conn->db->max_rows; i++) {
+      struct Address *row = &conn->db->rows[i];
+
+      if (row->name)
+        free(row->name);
+      if (row->email)
+        free(row->email);
+    }
+
+    if (conn->file)
+      fclose(conn->file);
+    if (conn->db)
+      free(conn->db);
+    free(conn);
+  }
+}
+
+void die(const char *message, struct Connection *conn) {
   if (errno) {
     perror(message);
   } else {
     printf("ERROR: %s\n", message);
   }
+  Database_close(conn);
   exit(1);
 }
 
@@ -37,19 +59,37 @@ void Address_print(struct Address *addr) {
 }
 
 void Database_load(struct Connection *conn) {
-  int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
-  if (rc != 1)
-    die("Failed to load database");
+  assert(conn->db && conn->file);
+
+  if (fread(conn->db, sizeof(struct Database), 1, conn->file) != 1)
+    die("Failed to load database", conn);
+
+  conn->db->rows = malloc(sizeof(struct Address) * conn->db->max_rows);
+
+  for (size_t i = 0; i < conn->db->max_rows; i++) {
+    struct Address *row = &conn->db->rows[i];
+    if (fread(&row->id, sizeof(int), 1, conn->file) != 1)
+      die("Failed to load id.", conn);
+    if (fread(&row->set, sizeof(int), 1, conn->file) != 1)
+      die("Failed to load set.", conn);
+
+    row->name = malloc(conn->db->max_data);
+    row->email = malloc(conn->db->max_data);
+    if (fread(row->name, conn->db->max_data, 1, conn->file) != 1)
+      die("Failed to load name", conn);
+    if (fread(row->email, conn->db->max_data, 1, conn->file) != 1)
+      die("Failed to load email", conn);
+  }
 }
 
 struct Connection *Database_open(const char *filename, char mode) {
   struct Connection *conn = malloc(sizeof(struct Connection));
   if (!conn)
-    die("Memory error");
+    die("Memory error", conn);
 
   conn->db = malloc(sizeof(struct Database));
   if (!conn->db)
-    die("Memory error");
+    die("Memory error", conn);
 
   if (mode == 'c') {
     conn->file = fopen(filename, "w");
@@ -60,37 +100,51 @@ struct Connection *Database_open(const char *filename, char mode) {
       Database_load(conn);
   }
   if (!conn->file)
-    die("Failed to open the file");
+    die("Failed to open the file", conn);
 
   return conn;
 }
 
-void Database_close(struct Connection *conn) {
-  if (conn) {
-    if (conn->file)
-      fclose(conn->file);
-    if (conn->db)
-      free(conn->db);
-    free(conn);
-  }
-}
-
 void Database_write(struct Connection *conn) {
   rewind(conn->file);
-  int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
-  if (rc != 1)
-    die("Failed to write database");
+  if (fwrite(conn->db, sizeof(struct Database), 1, conn->file) != 1)
+    die("Failed to write database", conn);
 
-  rc = fflush(conn->file);
-  if (rc == -1)
-    die("Cannot flush database");
+  for (size_t i = 0; i < conn->db->max_rows; i++) {
+    if (fwrite(&((conn->db->rows[i]).id), sizeof(int), 1, conn->file) != 1)
+      die("Failed to write id.", conn);
+    if (fwrite(&((conn->db->rows[i]).set), sizeof(int), 1, conn->file) != 1)
+      die("Failed to write set.", conn);
+    if (fwrite((conn->db->rows[i]).name, conn->db->max_data, 1, conn->file) !=
+        1)
+      die("Failed to write name.", conn);
+    if (fwrite((conn->db->rows[i]).email, conn->db->max_data, 1, conn->file) !=
+        1)
+      die("Failed to write email.", conn);
+  }
+
+  if (fflush(conn->file) == -1)
+    die("Cannot flush database", conn);
 }
 
 void Database_create(struct Connection *conn) {
-  int i = 0;
+  int max_rows, max_data;
+  printf("max_rows: ");
+  scanf("%d", &max_rows);
+  printf("max_data: ");
+  scanf("%d", &max_data);
+  conn->db->max_rows = max_rows;
+  conn->db->max_data = max_data;
 
-  for (i = 0; i < MAX_ROWS; i++) {
-    struct Address addr = {.id = i, .set = 0};
+  conn->db->rows =
+      (struct Address *)malloc(sizeof(struct Address) * conn->db->max_rows);
+
+  for (size_t i = 0; i < conn->db->max_rows; i++) {
+    char *name = (char *)malloc(conn->db->max_data);
+    memset(name, 0, conn->db->max_data);
+    char *email = (char *)malloc(conn->db->max_data);
+    memset(email, 0, conn->db->max_data);
+    struct Address addr = {.id = i, .set = 0, .name = name, .email = email};
     conn->db->rows[i] = addr;
   }
 }
@@ -98,17 +152,17 @@ void Database_create(struct Connection *conn) {
 void Database_set(struct Connection *conn, int id, char *name, char *email) {
   struct Address *addr = &conn->db->rows[id];
   if (addr->set)
-    die("Already set delete it frist");
+    die("Already set delete it frist", conn);
 
   name[MAX_DATA - 1] = '\0';
   char *res = strncpy(addr->name, name, MAX_DATA);
   if (!res)
-    die("Name copy failed");
+    die("Name copy failed", conn);
 
   email[MAX_DATA - 1] = '\0';
   res = strncpy(addr->email, email, MAX_DATA);
   if (!res)
-    die("Email copy failed");
+    die("Email copy failed", conn);
   addr->set = 1;
 }
 
@@ -117,18 +171,22 @@ void Database_get(struct Connection *conn, int id) {
   if (addr->set) {
     Address_print(addr);
   } else {
-    die("ID is not set");
+    die("ID is not set", conn);
   }
 }
 
 void Database_delete(struct Connection *conn, int id) {
-  struct Address addr = {.id = id, .set = 0};
+  char *name = (char *)malloc(conn->db->max_data);
+  memset(name, 0, conn->db->max_data);
+  char *email = (char *)malloc(conn->db->max_data);
+  memset(email, 0, conn->db->max_data);
+  struct Address addr = {.id = id, .set = 0, .name = name, .email = email};
   conn->db->rows[id] = addr;
 }
 
 void Database_list(struct Connection *conn) {
   struct Database *db = conn->db;
-  for (int i = 0; i < MAX_ROWS; i++) {
+  for (int i = 0; i < conn->db->max_rows; i++) {
     struct Address *cur = &db->rows[i];
     if (cur->set) {
       Address_print(cur);
@@ -138,15 +196,15 @@ void Database_list(struct Connection *conn) {
 
 int main(int argc, char *argv[]) {
   if (argc < 3)
-    die("USAGE: ex17 <dbfile> <action> [action params]");
-  char *filename = argv[1];
-  char action = argv[2][0];
-  struct Connection *conn = Database_open(filename, action);
+    die("USAGE: ex17 <dbfile> <action> [action params]", NULL);
   int id = 0;
   if (argc > 3)
     id = atoi(argv[3]);
-  if (id >= MAX_ROWS)
-    die("There's not that many records.");
+
+  char *filename = argv[1];
+  char action = argv[2][0];
+  struct Connection *conn = Database_open(filename, action);
+
   switch (action) {
   case 'c':
     Database_create(conn);
@@ -154,18 +212,18 @@ int main(int argc, char *argv[]) {
     break;
   case 'g':
     if (argc != 4)
-      die("Need an id to get");
+      die("Need an id to get", conn);
     Database_get(conn, id);
     break;
   case 's':
     if (argc != 6)
-      die("Need id, name, email to set");
+      die("Need id, name, email to set", conn);
     Database_set(conn, id, argv[4], argv[5]);
     Database_write(conn);
     break;
   case 'd':
     if (argc != 4)
-      die("Need id to delete");
+      die("Need id to delete", conn);
     Database_delete(conn, id);
     Database_write(conn);
     break;
@@ -173,7 +231,7 @@ int main(int argc, char *argv[]) {
     Database_list(conn);
     break;
   default:
-    die("Invalid action, only: c=create, g=get, s=set, d=del, l=list");
+    die("Invalid action, only: c=create, g=get, s=set, d=del, l=list", conn);
   }
   Database_close(conn);
 
--- exercises/ex17.test.sh
+++ exercises/ex17.test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 echo "-------- Without valgrind"
+rm db.dat
 echo "Creating database ..."
 ./ex17 db.dat c
 echo ""
@@ -28,29 +29,29 @@ echo ""
 echo "Getting record by id 2 ..."
 ./ex17 db.dat g 2
 
-echo "--------With valgrind"
+# echo "--------With valgrind"
 
-echo "Creating database ..."
-./ex17 db.dat c
-echo ""
+# echo "Creating database ..."
+# ./ex17 db.dat c
+# echo ""
 
-echo "Saving zed, frank, joe to database ..."
-valgrind ./ex17 db.dat s 1 zed zed@zedshaw.com
-valgrind ./ex17 db.dat s 2 frank frank@zedshaw.com
-valgrind ./ex17 db.dat s 3 joe joe@zedshaw.com
-echo ""
+# echo "Saving zed, frank, joe to database ..."
+# valgrind ./ex17 db.dat s 1 zed zed@zedshaw.com
+# valgrind ./ex17 db.dat s 2 frank frank@zedshaw.com
+# valgrind ./ex17 db.dat s 3 joe joe@zedshaw.com
+# echo ""
 
-echo "Printing all records ..."
-valgrind ./ex17 db.dat l
-echo ""
+# echo "Printing all records ..."
+# valgrind ./ex17 db.dat l
+# echo ""
 
-echo "Deleting record by id 3 ..."
-valgrind ./ex17 db.dat d 3
-echo ""
+# echo "Deleting record by id 3 ..."
+# valgrind ./ex17 db.dat d 3
+# echo ""
 
-echo "Printing all records ..."
-valgrind ./ex17 db.dat l
-echo ""
+# echo "Printing all records ..."
+# valgrind ./ex17 db.dat l
+# echo ""
 
-echo "Getting record by id 2 ..."
-valgrind ./ex17 db.dat g 2
+# echo "Getting record by id 2 ..."
+# valgrind ./ex17 db.dat g 2
